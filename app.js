6e6d787faa8b18d6960a9d02782684d409fe4676
Shruti Gupta <shruti122456@gmail.com>
2024-10-15 12:47:44 +0530

    new commit

--- app.js
+++ app.js
@@ -3,42 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// Middleware to parse the body of POST requests
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-// First middleware (this always runs)
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
-// Middleware to handle GET requests to /add-product
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send(`
-        <form action="/product" method="POST">
-            <input type="text" name="title" placeholder="Product Title"><br>
-            <input type="text" name="size" placeholder="Product Size"><br>
-            <button type="submit">Add Product</button>
-        </form>
-    `);
-});
-
-// Middleware to handle POST requests to /product
-app.use('/product', (req, res, next) => {
-    console.log('Product title:', req.body.title);  // Log product title
-    console.log('Product size:', req.body.size);    // Log product size
-    res.redirect('/');
-});
-
-// Final middleware to handle GET requests to the root '/'
-app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
-
-// Start the server on port 8000
-app.listen(8000, () => {
-    console.log('Server is running on port 8000');
-});
-
+app.listen(8000);
